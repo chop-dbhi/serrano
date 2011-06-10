a45618a9c3cc85b7dbdb0b8b5271379cda854677
Byron Ruth <ruthb@email.chop.edu>
2011-06-10 16:47:08 -0400

    cleaned up various code that is out of the scope of serrano

--- serrano/urls.py
+++ serrano/urls.py
@@ -1,15 +1,5 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
-    # authentication    
-    url(r'^login/$', 'serrano.views.login',
-        {'template_name': 'login.html'}, name='login'),
-    url(r'^logout/$', 'django.contrib.auth.views.logout_then_login',
-        name='logout'),
-
-    url(r'^define/$', 'serrano.views.define', name='define'),
-    url(r'^report/$', 'serrano.views.report', name='report'),
-
-    # API
     url(r'^api/', include('serrano.api.urls', namespace='api')),
 )
