87bab68ec1ad48a691b6a339c7038893ee985352
Byron Ruth <ruthb@email.chop.edu>
2011-06-10 16:43:12 -0400

    added package version utility. easier to maintain

--- serrano/__init__.py
+++ serrano/__init__.py
@@ -1,1 +1,14 @@
-__version__ = '1.0'
+VERSION = (2, 0, 0, 'beta', 1)
+
+def get_version():
+    version = '%s.%s' % VERSION[:2]
+    if version[2]:
+        version = '%s.%s' % (version, VERSION[2])
+
+    if len(VERSION) > 3:
+        if VERSION[3:] == ('alpha', 0):
+            version = '%spre-alpha' % version
+        else:
+            if VERSION[3] != 'final':
+                version = '%s%s%s' % (version, VERSION[3], VERSION[4])
+    return version
