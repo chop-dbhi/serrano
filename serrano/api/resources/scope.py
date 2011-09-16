1dbb6d71fcff29074fdd77123f8aa79cf6dd809d
Byron Ruth <bruth@codeomics.com>
2011-09-16 17:23:12 -0400

    Add initial PATCH support for incrementally adding/removing conditions Fix #3

--- serrano/api/resources/scope.py
+++ serrano/api/resources/scope.py
@@ -145,5 +145,68 @@ class SessionScopeResource(ScopeResource):
         return form.errors
 
 
+    def PATCH(self, request):
+        """Adds support for incrementally updating the Scope store. It handles
+        adding/removing a condition(s) for a single Concept.
+        """
+        scope = request.session['scope']
+        operation = request.data.get('operation', None)
+        condition = request.data.get('condition', None)
+
+        # both must exist to be processed
+        if not operation or not condition:
+            return http.UNPROCESSABLE_ENTITY
+
+        concept_id = condition['concept_id']
+
+        # TODO this logic assumes concept conditions are only first-level
+        # children. when concept conditions can be nested, this will need
+        # to be more robust at checking
+        if operation == 'remove':
+            # denotes a logical operator node e.g. AND | OR.
+            if scope.store.has_key('children'):
+                for i, x in enumerate(iter(scope.store['children'])):
+                    # TODO this logic assumes one condition per concept, update
+                    # this once this is not the case
+                    if x['concept_id'] == concept_id:
+                        scope.store['children'].pop(i)
+                        # move up the condition to the top node if it is the
+                        # last one
+                        if len(scope.store['children']) == 1:
+                            scope.store = scope.store['children'][0]
+                        break
+                else:
+                    return http.CONFLICT
+
+            # standalone condition
+            elif scope.store.get('concept_id') == concept_id:
+                scope.store = None
+
+            # a conflict in state between the client and the server
+            else:
+                return http.CONFLICT
+
+        # add operations must check the validity and permission of the
+        # requested content
+        elif operation == 'add':
+            # ensure the object is valid or fail
+            if not scope.is_valid(condition):
+                return http.UNPROCESSABLE_ENTITY
+
+            # ensure the user has permission to make use of corresponding fields
+            if not scope.has_permission(condition, request.user):
+                return http.UNAUTHORIZED
+
+            if not scope.store:
+                scope.store = condition
+            elif scope.store.has_key('children'):
+                scope.store['children'].append(condition)
+            else:
+                scope.store = {'type': 'and', 'children': [scope.store, condition]}
+
+        scope.save()
+        return self._condition(condition)
+
+
 class ScopeResourceCollection(resources.ModelResourceCollection):
     resource = ScopeResource
