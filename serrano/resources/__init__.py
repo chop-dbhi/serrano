52857539b476c5805fdb67acd4e1ddb3a1f8e019
Byron Ruth <b@devel.io>
2012-10-17 16:30:35 -0400

    Add root endpoint for discovering the main endpoints of the API

--- serrano/resources/__init__.py
+++ serrano/resources/__init__.py
@@ -1,1 +1,44 @@
+from django.conf.urls import patterns, url
+from django.core.urlresolvers import reverse
+from django.views.decorators.cache import never_cache
+from restlib2 import resources
 
+
+class Root(resources.Resource):
+    def get(self, request):
+        return {
+            'title': 'Serrano Hypermedia API',
+            '_links': {
+                'self': {
+                    'rel': 'self',
+                    'hrf': reverse('serrano:root'),
+                },
+                'fields': {
+                    'rel': 'datafields',
+                    'href': reverse('serrano:datafields'),
+                },
+                'concepts': {
+                    'rel': 'dataconcepts',
+                    'href': reverse('serrano:dataconcepts'),
+                },
+                'contexts': {
+                    'rel': 'datacontexts',
+                    'href': reverse('serrano:datacontexts'),
+                },
+                'views': {
+                    'rel': 'dataviews',
+                    'href': reverse('serrano:dataviews'),
+                },
+                'data': {
+                    'rel': 'data',
+                    'href': reverse('serrano:exporter'),
+                }
+            }
+        }
+
+
+root_resource = never_cache(Root())
+
+urlpatterns = patterns('',
+    url(r'^$', root_resource, name='root')
+)
--- serrano/urls.py
+++ serrano/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import patterns, url, include
 
+
 urlpatterns = patterns('',
     url(r'', include(patterns('',
+        url(r'^$', include('serrano.resources')),
         url(r'^fields/', include('serrano.resources.datafield')),
         url(r'^concepts/', include('serrano.resources.dataconcept')),
         url(r'^contexts/', include('serrano.resources.datacontext')),
