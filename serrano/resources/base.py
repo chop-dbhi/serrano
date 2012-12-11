a0fe7c8b8680f74645d2f387e97e74950e398a71
Byron Ruth <b@devel.io>
2012-12-11 10:12:13 -0500

    Refactor how a data context and data view is passed via an HTTP request GET and POST data is now supported directly for one-off queries. If neither of these are present, it falls back to checking for a primary key of a saved instance and then falls back to the session.

--- serrano/resources/base.py
+++ serrano/resources/base.py
@@ -11,60 +11,86 @@ class BaseResource(Resource):
             params.setdefault(param, default)
         return params
 
-    def get_context(self, request):
-        params = self.get_params(request)
-        context = params.get('context')
+    def get_context(self, request, attrs=None):
+        """Returns a DataContext object relative based on `attrs`.
 
-        # Explicit request to not use a context
-        if context != 'null':
-            kwargs = {
-                'archived': False,
-            }
-            if hasattr(request, 'user') and request.user.is_authenticated():
-                kwargs['user'] = request.user
-            else:
-                if request.session.session_key is None:
-                    return DataContext()
-                kwargs['session_key'] = request.session.session_key
+        If `attrs` is a dict, a temporary DataContext will be valiated
+        and created. Otherwise it is assumed `attrs` is a primary key
+        (or empty) and will return the appropriate DataContext.
+        """
+        if attrs is None:
+            if request.method == 'POST':
+                attrs = request.data.get('context')
+            elif request.method == 'GET':
+                attrs = request.GET.get('context')
 
-            # Assume it is a primary key and fallback to the sesssion
-            try:
-                kwargs['pk'] = int(context)
-            except (ValueError, TypeError):
-                kwargs['session'] = True
+        if isinstance(attrs, dict):
+            DataContext.validate(attrs)
+            return DataContext(json=attrs)
 
-            try:
-                return DataContext.objects.get(**kwargs)
-            except DataContext.DoesNotExist:
-                pass
+        # Assume a primary key or empty value
+        kwargs = {
+            'archived': False,
+        }
+
+        if hasattr(request, 'user') and request.user.is_authenticated():
+            kwargs['user'] = request.user
+        else:
+            if request.session.session_key is None:
+                return DataContext()
+            kwargs['session_key'] = request.session.session_key
+
+        # Assume it is a primary key and fallback to the sesssion
+        try:
+            kwargs['pk'] = int(attrs)
+        except (ValueError, TypeError):
+            kwargs['session'] = True
+
+        try:
+            return DataContext.objects.get(**kwargs)
+        except DataContext.DoesNotExist:
+            pass
 
         return DataContext()
 
-    def get_view(self, request):
-        params = self.get_params(request)
-        view = params.get('view')
+    def get_view(self, request, attrs=None):
+        """Returns a DataView object relative based on `attrs`.
 
-        # Explicit request to not use a view
-        if view != 'null':
-            kwargs = {
-                'archived': False,
-            }
-            if hasattr(request, 'user') and request.user.is_authenticated():
-                kwargs['user'] = request.user
-            else:
-                if request.session.session_key is None:
-                    return DataView()
-                kwargs['session_key'] = request.session.session_key
+        If `attrs` is a dict, a temporary DataView will be valiated
+        and created. Otherwise it is assumed `attrs` is a primary key
+        (or empty) and will return the appropriate DataView.
+        """
+        if attrs is None:
+            if request.method == 'POST':
+                attrs = request.data.get('view')
+            elif request.method == 'GET':
+                attrs = request.GET.get('view')
 
-            # Assume it is a primary key and fallback to the sesssion
-            try:
-                kwargs['pk'] = int(view)
-            except (ValueError, TypeError):
-                kwargs['session'] = True
+        if isinstance(attrs, dict):
+            DataView.validate(attrs)
+            return DataView(json=attrs)
 
-            try:
-                return DataView.objects.get(**kwargs)
-            except DataView.DoesNotExist:
-                pass
+        # Assume a primary key or empty valu
+        kwargs = {
+            'archived': False,
+        }
+
+        if hasattr(request, 'user') and request.user.is_authenticated():
+            kwargs['user'] = request.user
+        else:
+            if request.session.session_key is None:
+                return DataView()
+            kwargs['session_key'] = request.session.session_key
+
+        # Assume it is a primary key and fallback to the sesssion
+        try:
+            kwargs['pk'] = int(attrs)
+        except (ValueError, TypeError):
+            kwargs['session'] = True
+
+        try:
+            return DataView.objects.get(**kwargs)
+        except DataView.DoesNotExist:
+            pass
 
         return DataView()
