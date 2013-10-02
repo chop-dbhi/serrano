b2130d621cfe9ba997b9dbd1a3be791fc08cfef0
Don Naegely <naegelyd@gmail.com>
2013-10-02 13:54:47 -0400

    Add tests for PUT and DELETE requests on the context resource

--- tests/cases/resources/tests/context.py
+++ tests/cases/resources/tests/context.py
@@ -33,6 +33,61 @@ class ContextResourceTestCase(AuthenticatedBaseTestCase):
             HTTP_ACCEPT='application/json')
         self.assertEqual(response.status_code, codes.not_found)
 
+    def test_put(self):
+        # Add a context so we can try to update it later
+        ctx = DataContext(user=self.user, name='Context 1')
+        ctx.save()
+        response = self.client.get('/api/contexts/1/',
+            HTTP_ACCEPT='application/json')
+        self.assertEqual(response.status_code, codes.ok)
+        self.assertTrue(response.content)
+
+        # Attempt to update the name via a PUT request
+        response = self.client.put('/api/contexts/1/',
+            data=u'{"name":"New Name"}', content_type='application/json')
+        self.assertEqual(response.status_code, codes.no_content)
+
+        # Make sure our changes from the PUT request are persisted
+        response = self.client.get('/api/contexts/1/',
+            HTTP_ACCEPT='application/json')
+        self.assertEqual(response.status_code, codes.ok)
+        self.assertTrue(response.content)
+        self.assertEqual(json.loads(response.content)['name'], 'New Name')
+
+        # Make a PUT request with invalid JSON and make sure we get an
+        # unprocessable status code back.
+        response = self.client.put('/api/contexts/1/',
+            data=u'{"json":"]]]"}', content_type='application/json')
+        self.assertEqual(response.status_code, codes.unprocessable_entity)
+
+    def test_delete(self):
+        ctx = DataContext(user=self.user, name='Context 1')
+        ctx.save()
+        ctx = DataContext(user=self.user, name='Context 2')
+        ctx.save()
+        ctx = DataContext(user=self.user, name='Context 3', session=True)
+        ctx.save()
+
+        response = self.client.get('/api/contexts/',
+            HTTP_ACCEPT='application/json')
+        self.assertEqual(response.status_code, codes.ok)
+        self.assertEqual(len(json.loads(response.content)), 3)
+
+        response = self.client.delete('/api/contexts/1/')
+        self.assertEqual(response.status_code, codes.no_content)
+
+        response = self.client.get('/api/contexts/',
+            HTTP_ACCEPT='application/json')
+        self.assertEqual(response.status_code, codes.ok)
+        self.assertEqual(len(json.loads(response.content)), 2)
+
+        response = self.client.delete('/api/contexts/3/')
+        self.assertEqual(response.status_code, codes.bad_request)
+
+        response = self.client.get('/api/contexts/',
+            HTTP_ACCEPT='application/json')
+        self.assertEqual(response.status_code, codes.ok)
+        self.assertEqual(len(json.loads(response.content)), 2)
 
 
 class ContextsRevisionsResourceTestCase(AuthenticatedBaseTestCase):
