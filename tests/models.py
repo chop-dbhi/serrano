988d66f748adc781f81929007e8971eadf3aafb5
Don Naegely <naegelyd@gmail.com>
2013-10-07 13:15:25 -0400

    Add MockHandler for future testing of logging

--- tests/models.py
+++ tests/models.py
@@ -1,4 +1,23 @@
+import logging
 from django.db import models
+
+class MockHandler(logging.Handler):
+    def __init__(self, *args, **kwargs):
+        self.reset()
+        logging.Handler.__init__(self, *args, **kwargs)
+
+    def emit(self, record):
+        self.messages[record.levelname.lower()].append(record.getMessage())
+
+    def reset(self):
+        self.messages = {
+            'debug': [],
+            'info': [],
+            'warning': [],
+            'error': [],
+            'critical': [],
+        }
+
 
 class Office(models.Model):
     location = models.CharField(max_length=50)
