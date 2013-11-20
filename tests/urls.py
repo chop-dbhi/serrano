129e564dc3961fc464492e5aba14c682a49ff13e
Don Naegely <naegelyd@gmail.com>
2013-11-20 11:23:02 -0500

    Update tests to use a separate /results/ url from test urls
    This does a better job of recreating how the setting will be used in applications. Previously, we just linked to the api which would not generally be the desired behavior in applications using serrano.

--- tests/urls.py
+++ tests/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url, patterns, include
 
 
-urlpatterns = patterns('',
+urlpatterns = patterns(
+    '',
     url(r'^api/', include('serrano.urls')),
     url(r'^api/test/', include('tests.resources')),
+    url(r'^results/(?P<pk>\d+)/$', 'django.views.static.serve',
+        {'document_root': '/results'}, name='results'),
 )
