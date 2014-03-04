338e2ae2fe8004cad920f55aaecd72ed935eb3b4
Don Naegely <naegelyd@gmail.com>
2014-03-04 16:35:30 -0500

    Add more exporter resource tests
    Found these in a stash, might as well put them to use

--- tests/cases/resources/tests/exporter.py
+++ tests/cases/resources/tests/exporter.py
@@ -1,5 +1,6 @@
 import json
 from django.test import TestCase
+from restlib2.http import codes
 from avocado.conf import OPTIONAL_DEPS
 from serrano.resources import API_VERSION
 
@@ -7,8 +8,8 @@ from serrano.resources import API_VERSION
 class ExporterResourceTestCase(TestCase):
     def test_get(self):
         response = self.client.get('/api/data/export/',
-            HTTP_ACCEPT='application/json')
-        self.assertEqual(response.status_code, 200)
+                                   HTTP_ACCEPT='application/json')
+        self.assertEqual(response.status_code, codes.ok)
         self.assertEqual(response['Content-Type'], 'application/json')
 
         expectedResponse = {
@@ -47,3 +48,43 @@ class ExporterResourceTestCase(TestCase):
             }
 
         self.assertEqual(json.loads(response.content), expectedResponse)
+
+    def test_export_bad_type(self):
+        response = self.client.get('/api/data/export/bad_type/')
+        self.assertEqual(response.status_code, codes.not_found)
+
+    def test_export_all_pages(self):
+        response = self.client.get('/api/data/export/csv/')
+        self.assertEqual(response.status_code, codes.no_content)
+        self.assertTrue(response.get('Content-Disposition').startswith(
+            'attachment; filename="all'))
+        self.assertEqual(response.get('Content-Type'), 'text/csv')
+
+    def test_export_one_page(self):
+        response = self.client.get('/api/data/export/csv/1/')
+        self.assertEqual(response.status_code, codes.no_content)
+        self.assertTrue(response.get('Content-Disposition').startswith(
+            'attachment; filename="p1'))
+        self.assertEqual(response.get('Content-Type'), 'text/csv')
+
+    def test_export_page_range(self):
+        response = self.client.get('/api/data/export/csv/1...2/')
+        self.assertEqual(response.status_code, codes.no_content)
+        self.assertTrue(response.get('Content-Disposition').startswith(
+            'attachment; filename="p1-2'))
+        self.assertEqual(response.get('Content-Type'), 'text/csv')
+
+    def test_export_equal_page_range(self):
+        response = self.client.get('/api/data/export/csv/1...1/')
+        self.assertEqual(response.status_code, codes.no_content)
+        self.assertTrue(response.get('Content-Disposition').startswith(
+            'attachment; filename="p1'))
+        self.assertEqual(response.get('Content-Type'), 'text/csv')
+
+    def test_export_zero_page(self):
+        response = self.client.get('/api/data/export/csv/0/')
+        self.assertEqual(response.status_code, codes.not_found)
+
+    def test_export_bad_page_range(self):
+        response = self.client.get('/api/data/export/csv/3...1/')
+        self.assertEqual(response.status_code, codes.not_found)
