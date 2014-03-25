1823c5e3d8e828eaba4abe27d96159116048d69d
Byron Ruth <b@devel.io>
2014-03-25 11:00:13 -0400

    Update field value validation to use the native label field
    The minimum Avocado version is now 2.3.1 to take advantage of the new
    supplementary fields.
    
    Fix #172
    
    Signed-off-by: Byron Ruth <b@devel.io>

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import sys
 from setuptools import setup, find_packages
 
 install_requires = [
-    'avocado>=2.3,<2.4',
+    'avocado>=2.3.1,<2.4',
     'restlib2>=0.4.1,<0.5.0',
     'django-preserialize>=1.0.7,<1.1',
 ]
@@ -21,7 +21,7 @@ kwargs = {
 
     # Test dependencies
     'tests_require': [
-        'avocado[permissions,search,extras]>=2.3,<2.4'
+        'avocado[permissions,search,extras]>=2.3.1,<2.4'
         'coverage',
         'whoosh',
         'python-memcached>=1.48',
