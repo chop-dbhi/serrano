b3209089e63863a97abca2b44a36270ca8a18dfc
Don Naegely <naegelyd@gmail.com>
2015-05-03 21:59:10 -0700

    Call result row utility in query results endpoint
    Signed-off-by: Don Naegely <naegelyd@gmail.com>

--- serrano/resources/query/results.py
+++ serrano/resources/query/results.py
@@ -2,8 +2,9 @@ from django.http import Http404, HttpResponse
 from restlib2.params import IntParam
 
 from avocado.export import JSONExporter
-from avocado.query import pipeline, utils as query_utils
+from avocado.query import utils as query_utils
 from serrano.resources.query.base import QueryBase, QueryParametizer
+from ...utils import get_result_rows
 
 
 class QueryResultsParametizer(QueryParametizer):
@@ -43,84 +44,33 @@ class QueryResultsResource(QueryBase):
         return self.get_object(request, **kwargs) is None
 
     def get(self, request, **kwargs):
-        context = request.instance.context
-        view = request.instance.view
-
         params = self.get_params(request)
 
-        limit = params.get('limit')
-        tree = params.get('tree')
-
-        page = kwargs.get('page')
-        stop_page = kwargs.get('stop_page')
-
-        offset = None
-
-        if page:
-            page = int(page)
-
-            # Pages are 1-based.
-            if page < 1:
-                raise Http404
-
-            # Change to 0-base for calculating offset.
-            offset = limit * (page - 1)
-
-            if stop_page:
-                stop_page = int(stop_page)
-
-                # Cannot have a lower index stop page than start page.
-                if stop_page < page:
-                    raise Http404
-
-                # 4...5 means 4 and 5, not everything up to 5 like with
-                # list slices, so 4...4 is equivalent to just 4
-                if stop_page > page:
-                    limit = limit * stop_page
-        else:
-            # When no page or range is specified, the limit does not apply.
-            limit = None
-
-        QueryProcessor = pipeline.query_processors[params['processor']]
-        processor = QueryProcessor(context=context, view=view, tree=tree)
-        queryset = processor.get_queryset(request=request)
+        context = request.instance.context
+        view = request.instance.view
 
         # Isolate this query to a named connection. This will cancel an
         # outstanding queries of the same name if one is present.
         query_name = self.QUERY_NAME_TEMPLATE.format(pk=request.instance.pk)
-        query_utils.cancel_query(query_name)
-        queryset = query_utils.isolate_queryset(query_name, queryset)
 
-        exporter = processor.get_exporter(JSONExporter)
+        try:
+            rows, row_options = get_result_rows(
+                context,
+                view,
+                params.get('limit'),
+                params.get('tree'),
+                params.get('processor'),
+                kwargs.get('page'),
+                kwargs.get('stop_page'),
+                query_name,
+                params.get('reader'),
+                JSONExporter.short_name.lower()
+            )
+        except ValueError:
+            raise Http404
 
-        # This is an optimization when concepts are selected for ordering
-        # only. There is no guarantee to how many rows are required to get
-        # the desired `limit` of rows, so the query is unbounded. If all
-        # ordering facets are visible, the limit and offset can be pushed
-        # down to the query.
-        order_only = lambda f: not f.get('visible', True)
-        view_node = view.parse()
         resp = HttpResponse()
-
-        if filter(order_only, view_node.facets):
-            iterable = processor.get_iterable(queryset=queryset,
-                                              request=request)
-
-            # Write the data to the response
-            exporter.write(iterable,
-                           resp,
-                           request=request,
-                           offset=offset,
-                           limit=limit)
-        else:
-            iterable = processor.get_iterable(queryset=queryset,
-                                              request=request,
-                                              limit=limit,
-                                              offset=offset)
-
-            exporter.write(iterable,
-                           resp,
-                           request=request)
+        row_options['exporter'].write(rows, buff=resp, request=request)
 
         return resp
 
