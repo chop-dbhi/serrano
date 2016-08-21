756e0b12679293ccab3a56fdd36a3997e39ab41f
Byron Ruth <b@devel.io>
2016-08-21 08:20:35 -0400

    Expose required headers for CORS requests
    The Link and Link-Template headers are necessary for clients to discover
    linked resources. Per the CORS spec, these must be allowed explicitly by
    the server.
    
    Signed-off-by: Byron Ruth <b@devel.io>

--- serrano/cors.py
+++ serrano/cors.py
@@ -1,5 +1,14 @@
 from serrano.conf import settings
 
+# Response headers the server explicitly allows cross-origin agents to access.
+# The default set based on the spec are: cache-control, content-language,
+# content-type, expires, last-modified, and pragma.
+exposed_headers = (
+    'Content-Length',
+    'Link',
+    'Link-Template',
+)
+
 
 def is_preflight(request):
     return (request.method == 'OPTIONS' and
@@ -17,9 +26,12 @@ def patch_response(request, response, methods):
             # See https://developer.mozilla.org/en-US/docs/HTTP/Access_control_CORS#Access-Control-Allow-Origin # noqa
             response['Access-Control-Allow-Origin'] = origin
             response['Access-Control-Allow-Credentials'] = 'true'
+            response['Access-Control-Expose-Headers'] = ', '.join(exposed_headers)  # noqa
+
             if request.method == 'OPTIONS':
                 response['Access-Control-Allow-Methods'] = ', '.join(methods)
-                headers = request.META.get('HTTP_ACCESS_CONTROL_REQUEST_HEADERS')  # noqa
+                headers = request.META.get('HTTP_ACCESS_CONTROL_REQUEST_HEADERS', '')  # noqa
                 if headers:
                     response['Access-Control-Allow-Headers'] = headers
+
     return response
